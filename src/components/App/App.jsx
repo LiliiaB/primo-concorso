966cab50497fc62732653a997444048e5bcf2097
Lilia Levchenko <liliya.benatti@gmail.com>
2024-09-01 17:48:04 +0200

    structure update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import NavBar from "../NavBar/NavBar";
+import { NavBar } from "../NavBar/NavBar";
 import { Loader } from "../Loader/Loader";
+import { Footer } from "../Footer/Footer";
+
+const HomePage = lazy(() => import("../../Pages/HomePage"));
+const TermsPage = lazy(() => import("../../Pages/TermsPage"));
+const GalleryPage = lazy(() => import("../../Pages/GalleryPage"));
 
 export default function App() {
   return (
@@ -16,6 +21,7 @@ export default function App() {
           <Route path="/terms" element={<TermsPage />} />
           <Route path="*" element={<HomePage />} />
         </Routes>
+        <Footer />
       </Suspense>
     </>
   );
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Go back</Link>
+      </li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+};
+
+export default NavBar;
