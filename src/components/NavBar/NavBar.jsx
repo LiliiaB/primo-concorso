83390f4333a6cb81853cca4a0784ea546f51f492
Lilia Levchenko <liliya.benatti@gmail.com>
2024-09-01 19:26:29 +0200

    nav bar done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../images/logo.png";
+import css from "./NavBar.module.css";
+import clsx from "clsx";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export const NavBar = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">
-          <img
-            src={logo}
-            alt="Logo"
-            style={{ width: "50px", height: "50px" }}
-          />
-        </Link>
-      </li>
-      <li>Gallery</li>
-      <li>Terms and conditions</li>
-    </ul>
+    <nav className={css.nav}>
+      <Link to="/"></Link>
+
+      <NavLink to="/" className={buildLinkClass}>
+        <img src={logo} alt="Logo" style={{ width: "60px", height: "60px" }} />
+      </NavLink>
+      <NavLink to="/gallery" className={buildLinkClass}>
+        Gallery
+      </NavLink>
+      <NavLink to="/terms" className={buildLinkClass}>
+        Terms and conditions
+      </NavLink>
+    </nav>
   );
 };
 
