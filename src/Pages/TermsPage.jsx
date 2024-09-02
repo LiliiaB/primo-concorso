e50d4a415223d14cae6f2aa26fecc577456d4b8c
Lilia Levchenko <liliya.benatti@gmail.com>
2024-09-02 21:20:58 +0200

    terms page added

--- src/Pages/TermsPage.jsx
+++ src/Pages/TermsPage.jsx
@@ -1,7 +1,91 @@
+import css from "./TermsPage.module.css";
+
 export default function TermsPage() {
   return (
-    <>
-      <div>Terms Page</div>
-    </>
+    <div className={css.container}>
+      <h2>Terms and Conditions</h2>
+      <div>
+        <h3>Titolo della mostra: </h3>
+        <p>"Il mondo dei colori dei bambini: Ispirazione e creatività"</p>
+        <h3>Luogo:</h3>
+        <p>
+          Galleria «Veneto Art Gallery», via Giuseppe Garibaldi, 23, Rosà (Vi){" "}
+        </p>
+        <h3>Date: </h3>
+        <p>01-16 Giugno 2024</p>
+        <h3>Descrizione del progetto:</h3>
+        <p>
+          Questa mostra, organizzata dalla Galleria «Veneto Art Gallery», è una
+          celebrazione della creatività infantile espressa su carta ed altri
+          materiali. Vengono esposti disegni e lavoretti dei bambini provenienti
+          da varie scuole, associazioni e piccoli artisti che condividono con
+          gioia le loro visioni con diversi materiali, tra cui anche la
+          plastilina e l'argilla.
+        </p>
+        <h3>Tema della mostra:</h3>
+        <p>
+          "Ispirazione e creatività" - un tema motivante ispirato dalle parole
+          di Picasso: "Per dipingere come Raffaello, ho studiato per quattro
+          anni, ma ci ho messo una vita per imparare a dipingere come un
+          bambino." Questa citazione funge da fondamento filosofico, ispirando
+          l'esplorazione del mondo attraverso gli occhi dei bambini.{" "}
+        </p>
+        <h3> Partecipanti: </h3>
+        <p>
+          Associazioni culturali e bambini di diverse età che disegnano nel
+          tempo libero. Questa diversità consente di esporre una straordinaria
+          varietà di opere creative, riflettendo prospettive e approcci
+          artistici diversi.
+        </p>
+        <h3> Programma:</h3>
+        <ul>
+          <li>
+            Apertura della mostra: Incontro con i partecipanti, genitori ed
+            educatori.
+          </li>
+          <li>
+            aboratori creativi: i bambini possono partecipare a laboratori
+            creativi in loco per continuare il loro siluppo artistico.
+          </li>
+          <li>
+            Finissaggi: Concorsi e premi: Competizioni che sottolineano
+            l'unicità di ogni opera
+          </li>
+        </ul>
+        <ul>
+          <li>Primo premio: 100 Euro</li>
+          <li>Secondo premio: 75 Euro</li>
+          <li> Terzo premio: 50 Euro</li>
+          <li>Premi dagli Sponsor </li>
+        </ul>
+        <h3>
+          Per la partecipazione è richiesto un piccolo contributo in base al
+          numero delle opere presentate.
+        </h3>
+        <h3>
+          Le opere devono essere consegnate alla Galleria dal 15 al 20 Maggio.
+        </h3>
+        <h3> Si consiglia di incorniciare i disegni con passe-partout.</h3>
+        <h3>
+          Ogni opera deve essere accompagnata da una scheda indicante l'autore,
+          l'età, e il luogo di residenza.
+        </h3>
+        <h3>
+          I disegni potranno essere ritirati entro tre giorni dalla fine della
+          mostra o spediti per posta previo accordo.
+        </h3>
+        <p>
+          Esclusione della responsabilità Per l'intera durata del Concorso il
+          rischio di danneggiamento o perimento dell'opera rimarrà a carico
+          esclusivo dell'Artista e non della Galleria la quale declina sin d'ora
+          ogni forma di responsabilità relativa alla custodia dell'opera.
+          Galleria «Veneto Art Gallery» sarà altresì esonerata da qualsiasi
+          responsabilità in caso di furto dell'opera nella Mostra. Questa mostra
+          mira a creare una piattaforma per l'ispirazione, lo scambio di idee e
+          la celebrazione del potenziale creativo dei bambini presso la Galleria
+          «Veneto Art Gallery».
+        </p>
+      </div>
+    </div>
   );
 }
